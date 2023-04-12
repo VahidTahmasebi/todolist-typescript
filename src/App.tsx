1455d74b11dc787ad39a7e87549bb40ab11cad6e
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-04-12 16:17:47 +0430

    Add structure app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div>
+      <header>
+        <div className='navbar navbar-dark bg-dark shadow-sm'>
+          <div className='container d-flex justify-content-between'>
+            <a href='#' className='navbar-brand d-flex align-items-center'>
+              <strong>Todo App</strong>
+            </a>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className='jumbotron'>
+          <div className='container d-flex flex-column align-items-center'>
+            <h1 className='jumbotron-heading'>Welcome!</h1>
+            <p className='lead text-muted'>
+              To get started, add some items to your list:
+            </p>
+          </div>
+        </section>
+        <div className='todosList'>
+          <div className='container'>
+            <div className='d-flex flex-column align-items-center'>
+              <nav className='col-6 mb-3'>
+                <div id='nav-tab' role='tablist' className='nav nav-tabs'>
+                  <a
+                    href=''
+                    id='nav-home-tab'
+                    className={`nav-item nav-link font-weight-bold`}>
+                    undone
+                    <span className='badge badge-secondary'>9</span>
+                  </a>
+                  <a
+                    href=''
+                    id='nav-home-tab'
+                    className={`nav-item nav-link font-weight-bold`}>
+                    done
+                    <span className='badge badge-success'>9</span>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
